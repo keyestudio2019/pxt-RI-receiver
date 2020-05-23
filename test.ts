19decad15fef8bad43d6d47ce359a7d2a2ca7855
mworkfun <1123552240@qq.com>
2020-05-23 08:31:44 +0800

    update

--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
 // tests go here; this will not be compiled when this package is used as a library
+
+irReceiver.irButton(0)
+basic.forever(() => {
+    3
+    irReceiver.pressedIrButton()
+    4
+    })
