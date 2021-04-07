ff299a45aa54b3828c09ca37d6827eeea8c0f9b1
keyestudio <53899501+keyestudio2019@users.noreply.github.com>
2021-04-07 09:43:17 +0800

    Update pxt.json, shims.d.ts, enums.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -7,25 +7,4 @@
 declare namespace maqueenIRV2 {
 }
 
-
-
-    //% color=50 weight=80
-    //% icon="\uf1eb"
-declare namespace keyestudioIR {
-
-    /**
-     * button pushed.
-     */
-    //% blockId=ir_received_left_event
-    //% block="on |%btn| button pressed" shim=keyestudioIR::onPressEvent
-    function onPressEvent(btn: RemoteButton, body: () => void): void;
-
-    /**
-     * initialises local variablesssss
-     */
-    //% blockId=ir_init
-    //% block="connect ir receiver to %pin" shim=keyestudioIR::initIR
-    function initIR(pin: Pins): void;
-}
-
 // Auto-generated. Do not edit. Really.
